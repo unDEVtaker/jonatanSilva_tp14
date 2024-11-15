a1ca468e7772cdf6c762c030e41a0e661042eef3
Jona Silva <jonatanemanuelsilva6@gmail.com>
2024-11-15 00:16:36 -0300

    add terminado microdesafio2 movies.js

--- movies.js
+++ movies.js
@@ -16,3 +16,43 @@ Nos piden crear una estructura parecida a la trabajada en el punto 2, teniendo e
 En todos los casos se debe mostrar por consola el resultado obtenido
 */
 
+//2.Crear el array 
+let peliculas = ['Turno de día', '30 noches con mi ex', 'Bestia', 'El monte', 'Top gun maverick', 'Elvis', 'Thor: amor y trueno'];
+
+//3.funcion para mover thor al primer lugar y en mayusculas
+
+function moverPeliculaAlInicio(peliculas, nombrePelicula) {
+    // Convertir la película a mayúsculas
+    const peliculaEnMayusculas = nombrePelicula.toUpperCase();
+    
+    // Encontrar el índice de la película
+    const indice = peliculas.indexOf(nombrePelicula);
+    
+    // Si la película se encuentra, la elimino pero la agrego al inicio
+    if (indice !== -1) {
+        peliculas.splice(indice, 1);
+        peliculas.unshift(peliculaEnMayusculas);
+    }
+    return peliculas;
+}
+
+//llamo a la funcion y muestro por consola
+peliculas = moverPeliculaAlInicio(peliculas, 'Thor: amor y trueno');
+console.log('Películas ordenadas:', peliculas);
+
+//3.Crear una cadena de texto con las películas próximas a estrenar
+const proximosEstrenos = 'Counter-Strike, NOP, Vértigo, Nick, Avatar';
+
+// Convertir la cadena en una estructura de array como en el punto 2 y separo por comas
+const proximosEstrenosArray = proximosEstrenos.split(', ');
+console.log('Proximos Estrenos:', proximosEstrenosArray);
+
+//4. crear un funcion para combinar ambas estructuras
+function combinarPeliculas(listaPeliculas1, listaPeliculas2) {
+    //combinar las estructuras usando .concat()
+    return listaPeliculas1.concat(listaPeliculas2);
+}
+
+//llamo a la funcion y muetro por consola
+const listadoCompletoDePeliculas = combinarPeliculas(peliculas, proximosEstrenosArray);
+console.log('Todas las películas:', listadoCompletoDePeliculas);
